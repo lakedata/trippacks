3b6ccd15eda35fd049edd335421db20d56916b28
jung <wldud2550@naver.com>
2023-09-17 07:13:48 +0900

    feat : Springboot 환경설정

---
+++ src/test/java/dwu/swcmop/trippacks/TrippacksApplicationTests.java
@@ -0,0 +1,13 @@
+package dwu.swcmop.trippacks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrippacksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
