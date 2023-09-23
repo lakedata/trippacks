14d3dc335b298892ab930701fc1a628b87446b51
jung <wldud2550@naver.com>
2023-09-23 16:17:34 +0900

    feat : kakao-user

--- src/main/java/dwu/swcmop/trippacks/repository/UserRepository.java
+++ src/main/java/dwu/swcmop/trippacks/repository/UserRepository.java
@@ -4,5 +4,10 @@ import dwu.swcmop.trippacks.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    User save(User user);
+//    User save(User user);
+    // JPA findBy 규칙
+    // select * from user_master where kakao_email = ?
+    public User findByKakaoEmail(String kakaoEmail);
+
+    public User findByUserCode(String userCode);
 }
--- src/main/java/dwu/swcmop/trippacks/service/UserService.java
+++ src/main/java/dwu/swcmop/trippacks/service/UserService.java
@@ -1,16 +1,65 @@
 package dwu.swcmop.trippacks.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import dwu.swcmop.trippacks.entity.User;
+import dwu.swcmop.trippacks.model.OauthToken;
 import dwu.swcmop.trippacks.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
 
+
+@Service
 public class UserService {
-    private final UserRepository userRepository;
+    @Autowired
+    UserRepository userRepository; //(1)
 
-    public UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
+    public OauthToken getAccessToken(String code) {
 
-    public void delete(User user) {
-        userRepository.delete(user);
+        //(2)RsTemplate이용해 URL형식으로 PSOT
+        RestTemplate rt = new RestTemplate();
+
+        //(3)헤더만들기
+        //(3)
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+
+        //(4)
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("grant_type", "authorization_code");
+        params.add("client_id", "b791159adc4e18ab175997922e03859a");//{클라이언트 앱 키}
+        params.add("redirect_uri", "http://localhost:8080/auth");//{리다이렉트 uri}
+        params.add("code", code);
+        params.add("client_secret", "SUMeEShOhtICwtY23mlES55AY66d97pP"); // 생략 가능!{시크릿 키}
+
+        //(5)
+        HttpEntity<MultiValueMap<String, String>> kakaoTokenRequest =
+                new HttpEntity<>(params, headers);
+
+        //(6)
+        ResponseEntity<String> accessTokenResponse = rt.exchange(
+                "https://kauth.kakao.com/oauth/token",
+                HttpMethod.POST,
+                kakaoTokenRequest,
+                String.class
+        );
+
+        //(7)
+        ObjectMapper objectMapper = new ObjectMapper();
+        OauthToken oauthToken = null;
+        try {
+            oauthToken = objectMapper.readValue(accessTokenResponse.getBody(), OauthToken.class);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+        return oauthToken; //(8)
     }
 }
