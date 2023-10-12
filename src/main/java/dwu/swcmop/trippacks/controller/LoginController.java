b42ef2afee6805fe162aa395a40b4cfb901ed7ac
jung <wldud2550@naver.com>
2023-10-12 21:43:49 +0900

    feat : Kakao회원탈퇴/사용자 조회

--- src/main/java/dwu/swcmop/trippacks/controller/LoginController.java
+++ src/main/java/dwu/swcmop/trippacks/controller/LoginController.java
@@ -1,8 +1,11 @@
 package dwu.swcmop.trippacks.controller;
 
+import dwu.swcmop.trippacks.config.BaseException;
+import dwu.swcmop.trippacks.config.BaseResponse;
 import dwu.swcmop.trippacks.entity.User;
 import dwu.swcmop.trippacks.config.jwt.JwtProperties;
 import dwu.swcmop.trippacks.service.UserService;
+import dwu.swcmop.trippacks.repository.UserRepository;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
@@ -11,14 +14,17 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 
+import static dwu.swcmop.trippacks.config.BaseResponseStatus.INVALID_USER;
+
 @RestController //(1)
-@RequestMapping("/api")
+@RequestMapping("/kakao")
 public class LoginController {
     @Autowired
     private UserService userService; //(2)
 
+
     // 프론트에서 인가코드 받아오는 url
-    @ApiOperation(value = "카카오회원가입", notes = "access_token을 얻어 db저장 후 jwt발급한다.")
+    @ApiOperation(value = "카카오 로그인", notes = "access_token을 얻어 db저장 후 jwt발급한다.")
     @GetMapping("/oauth/token") // (3)
     public ResponseEntity getLogin(@RequestParam("code") String code) { //(4)
         System.out.println("code : " + code);
@@ -34,6 +40,40 @@ public class LoginController {
         return ResponseEntity.ok().headers(headers).body("success");
     }
 
+    @ApiOperation(value = "카카오 회원탈퇴", notes = "JWT로 사용자id 받아와 DB삭제")
+    @DeleteMapping("/delete")
+    public BaseResponse<String> deleteUser(@RequestParam("Authorization") String accessToken) {
+        try {
+            Long Id = userService.extractId(accessToken);
+            userService.deleteUser(Id);
+
+            String result = "삭제되었습니다.";
+
+            return new BaseResponse<>(result);
+
+        } catch (BaseException exception) {
+            return new BaseResponse<>((exception.getStatus()));
+        }
+    }
+
+    @ApiOperation(value = "사용자 조회", notes = "사용자Id로 사용자정보 조회")
+    @GetMapping("/{userId}")
+    public BaseResponse<String> getUser(@PathVariable("userId") Long id) {
+
+        try {
+            String user = String.valueOf(userService.findById(id));
+
+            if (userService.findById(id) == null) {
+                return new BaseResponse<>(INVALID_USER);
+            }
+
+            return new BaseResponse<>(user);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
     // jwt 토큰으로 유저정보 요청하기
     @ApiOperation(value = "사용자 정보", notes = "HttpServletRequest로 사용자 정보 가져오기.")
     @GetMapping("/me")
@@ -43,6 +83,8 @@ public class LoginController {
 
         return ResponseEntity.ok().body(user);
     }
+
+
 
 
 }
--- src/main/java/dwu/swcmop/trippacks/service/BagService.java
+++ src/main/java/dwu/swcmop/trippacks/service/BagService.java
@@ -14,6 +14,7 @@ import static dwu.swcmop.trippacks.config.BaseResponseStatus.SAVE_BAG_FAIL;
 public class BagService {
     @Autowired
     private BagRepository bagRepository;
+
     public BagService(BagRepository bagRepository) {
         this.bagRepository = bagRepository;
     }
