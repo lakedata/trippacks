9d748cede756d532d8035b00c61d0d35358ad2f5
jung <wldud2550@naver.com>
2023-10-15 22:32:19 +0900

    Enhancement: 보안 개선 - 민감한 정보를 Git Secrets로 이동

--- src/main/java/dwu/swcmop/trippacks/config/ChatGptConfig.java
+++ src/main/java/dwu/swcmop/trippacks/config/ChatGptConfig.java
@@ -1,9 +1,13 @@
 package dwu.swcmop.trippacks.config;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class ChatGptConfig {
     public static final String AUTHORIZATION = "Authorization";
     public static final String BEARER = "Bearer ";
-    public static final String API_KEY = "123"; //실행시 원래 api key로 바꾸고, git에 올릴 때에는 임의 값으로 다시 설정!
+
+    @Value("${chatGpt.key}")
+    public static String API_KEY; //실행시 원래 api key로 바꾸고, git에 올릴 때에는 임의 값으로 다시 설정!
     public static final String MODEL = "text-davinci-003";
     public static final Integer MAX_TOKEN = 300;
     public static final Double TEMPERATURE = 0.0;
