efe2b3cb200b1e095c296d24404a15369660b2ba
jung <wldud2550@naver.com>
2023-11-25 02:34:39 +0900

    fix : BagList 페이징처리

--- src/main/java/dwu/swcmop/trippacks/repository/BagRepository.java
+++ src/main/java/dwu/swcmop/trippacks/repository/BagRepository.java
@@ -3,6 +3,8 @@ package dwu.swcmop.trippacks.repository;
 import dwu.swcmop.trippacks.dto.BagStatus;
 import dwu.swcmop.trippacks.entity.Bag;
 import dwu.swcmop.trippacks.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -11,6 +13,7 @@ import java.util.Optional;
 public interface BagRepository extends JpaRepository<Bag, Long> {
     public List<Bag> findAllByKakaoId(Long kakaoId);
     public Bag findByBagId(Long id);
-    List<Bag> findAllByKakaoIdAndStatus(Long kakaoId, BagStatus status);
+    List<Bag> findByKakaoIdAndStatus(Long kakaoId, BagStatus status);
+    //    List<Bag> findAllByKakaoIdAndStatus(Long kakaoId, BagStatus status);
 
 }
